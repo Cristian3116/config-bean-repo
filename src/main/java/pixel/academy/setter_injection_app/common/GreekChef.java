724e019010bb17b02289e3544c7c0cca00056721
CristianM <mija.cristian3101@gmail.com>
2025-11-26 18:27:56 +0200

    feat: add class GreekChef

---
+++ src/main/java/pixel/academy/setter_injection_app/common/GreekChef.java
@@ -0,0 +1,12 @@
+package pixel.academy.setter_injection_app.common;
+
+public class GreekChef implements Chef {
+    public GreekChef() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyRecipe() {
+        return "Greek Salad with fresh ingredients";
+    }
+}
